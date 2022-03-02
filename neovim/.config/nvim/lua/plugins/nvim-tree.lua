28ca195379dd9d865bef014d30e90bd8a04d7bf3
Marcin Paliwoda <paliwoda.marcin@zoho.com>
2022-03-02 14:53:06 +0100

    Update nvim-tree config

--- neovim/.config/nvim/lua/plugins/nvim-tree.lua
+++ neovim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -1,19 +1,15 @@
 local g = vim.g
 
-g.nvim_tree_indent_markers = 1
+g.nvim_tree_indent_markers = 0
 g.nvim_tree_git_hl = 1
 g.nvim_tree_highlight_opened_files = 1
 g.nvim_tree_root_folder_modifier = '>>>'
-g.nvim_tree_quit_on_open = 0
 g.nvim_tree_add_trailing = 0
 g.nvim_tree_group_empty = 1
-g.nvim_tree_change_dir_global = 1
-g.nvim_tree_disable_window_picker = 0
 g.nvim_tree_icon_padding = ' '
 g.nvim_tree_respect_buf_cwd = 0
 g.nvim_tree_create_in_closed_folder = 1
 g.nvim_tree_refresh_wait = 1000
-g.nvim_tree_window_picker_exclude = { buftype = { 'terminal' } }
 
 g.nvim_tree_special_files = { ['README.md'] = 1, Makefile = 1, MAKEFILE = 1 }
 g.nvim_tree_show_icons = {
@@ -42,6 +38,20 @@ ntree.setup {
   update_to_buf_dir   = {
     enable = false,
     auto_open = false,
+  },
+  actions = {
+    open_file = {
+        quit_on_open = false,
+        window_picker = {
+            enable = true,
+            exclude = {
+                buftype = {'terminal'}
+            },
+        }
+    },
+    change_dir = {
+        global = true,
+    }
   },
   diagnostics = {
     enable = true,
