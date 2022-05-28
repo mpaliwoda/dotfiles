5b694b160a0a75aa693e69cb7af93d3033180981
Marcin Paliwoda <paliwoda.marcin@zoho.com>
2022-05-28 16:51:50 +0200

    Move nvim-tree settings from vim vars to lua setup, reformat file

--- neovim/.config/nvim/lua/plugins/nvim-tree.lua
+++ neovim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -1,22 +1,6 @@
 local g = vim.g
 
-g.nvim_tree_git_hl = 1
-g.nvim_tree_highlight_opened_files = 1
-g.nvim_tree_root_folder_modifier = '>>>'
-g.nvim_tree_add_trailing = 0
-g.nvim_tree_group_empty = 1
-g.nvim_tree_icon_padding = ' '
-g.nvim_tree_respect_buf_cwd = 0
-g.nvim_tree_create_in_closed_folder = 1
 g.nvim_tree_refresh_wait = 1000
-
-g.nvim_tree_special_files = { ['README.md'] = 1, Makefile = 1, MAKEFILE = 1 }
-g.nvim_tree_show_icons = {
-    git = 1,
-    folders = 1,
-    files = 1,
-    folder_arrows = 0,
-}
 
 
 local present, ntree = pcall(require, 'nvim-tree')
@@ -26,75 +10,90 @@ end
 
 
 ntree.setup {
-  disable_netrw       = true,
-  hijack_netrw        = true,
-  open_on_setup       = false,
-  ignore_ft_on_setup  = {},
-  open_on_tab         = false,
-  hijack_cursor       = false,
-  update_cwd          = false,
-  actions = {
-    open_file = {
-        quit_on_open = false,
-        window_picker = {
-            enable = true,
-            exclude = {
-                buftype = {'terminal'}
-            },
+    disable_netrw           = true,
+    hijack_netrw            = true,
+    open_on_setup           = false,
+    ignore_ft_on_setup      = {},
+    open_on_tab             = false,
+    hijack_cursor           = false,
+    update_cwd              = false,
+    respect_buf_cwd         = false,
+    create_in_closed_folder = true,
+    actions                 = {
+        open_file = {
+            quit_on_open = false,
+            window_picker = {
+                enable = true,
+                exclude = {
+                    buftype = { 'terminal' }
+                },
+            }
+        },
+        change_dir = {
+            global = true,
         }
     },
-    change_dir = {
-        global = true,
-    }
-  },
-  diagnostics = {
-    enable = true,
-    icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = "",
-    }
-  },
-  update_focused_file = {
-    enable      = false,
-    update_cwd  = false,
-    ignore_list = {}
-  },
-  system_open = {
-    cmd  = nil,
-    args = {}
-  },
-  filters = {
-    dotfiles = false,
-    custom = {}
-  },
-  git = {
-    enable = true,
-    ignore = true,
-    timeout = 500,
-  },
-  view = {
-    width = 35,
-    height = 100,
-    hide_root_folder = false,
-    side = 'left',
-    mappings = {
-      custom_only = false,
-      list = {
-        { key = "v", action = "vsplit" },
-        { key = "s", action = "split" },
-      }
+    diagnostics             = {
+        enable = true,
+        icons = {
+            hint = "",
+            info = "",
+            warning = "",
+            error = "",
+        }
     },
-    number = true,
-    relativenumber = true,
-    signcolumn = "yes"
-  },
-  trash = {
-    cmd = "trash",
-    require_confirm = true
-  },
-  renderer = {
-      indent_markers = { enable = false }
-  }
+    update_focused_file     = {
+        enable      = false,
+        update_cwd  = false,
+        ignore_list = {}
+    },
+    system_open             = {
+        cmd  = nil,
+        args = {}
+    },
+    filters                 = {
+        dotfiles = false,
+        custom = {}
+    },
+    git                     = {
+        enable = true,
+        ignore = true,
+        timeout = 500,
+    },
+    view                    = {
+        width = 35,
+        height = 100,
+        hide_root_folder = false,
+        side = 'left',
+        mappings = {
+            custom_only = false,
+            list = {
+                { key = "v", action = "vsplit" },
+                { key = "s", action = "split" },
+            }
+        },
+        number = true,
+        relativenumber = true,
+        signcolumn = "yes"
+    },
+    trash                   = {
+        cmd = "trash",
+        require_confirm = true
+    },
+    renderer                = {
+        indent_markers         = { enable = false },
+        highlight_git          = true,
+        highlight_opened_files = "all",
+        root_folder_modifier   = ">>>",
+        add_trailing           = false,
+        group_empty            = false,
+        icons                  = { padding = ' ', show = {
+            git = true,
+            folder = true,
+            file = true,
+            folder_arrow = true,
+        } },
+        special_files          = { ['README.md'] = 1, Makefile = 1, MAKEFILE = 1 }
+
+    }
 }
