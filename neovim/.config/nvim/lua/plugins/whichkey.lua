da140ea6165892a6bffa39c9d0fb8982a7733b21
Marcin Paliwoda <paliwoda.marcin@zoho.com>
2022-07-06 13:12:30 -0700

    Add imperfect which-key descriptions

--- neovim/.config/nvim/lua/plugins/whichkey.lua
+++ neovim/.config/nvim/lua/plugins/whichkey.lua
@@ -4,4 +4,25 @@ if not present then
     return
 end
 
+whichkey.register(
+    {
+        a  = { name = "Debugging" },
+        c  = { name = "Coc Telescope" },
+        f  = { name = "Fuzzy Finding" },
+        g  = { name = "Git" },
+        m  = { name = "Coc" },
+        r  = { name = "Refactoring" },
+        s  = { name = "Search & Replace" },
+        t  = { name = "Terminal" },
+        tf = { name = "Terminal: Floating" },
+        u  = { name = "Undo" },
+        i  = { name = "Sort imports" },
+        w  = { name = "Tasks" },
+    },
+    {
+        prefix = "<leader>"
+    }
+)
+
+
 whichkey.setup()
