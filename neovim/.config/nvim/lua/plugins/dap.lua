b61a37ec77fa12166c836f4d110935b7af542897
Marcin Paliwoda <paliwoda.marcin@zoho.com>
2022-07-06 13:13:14 -0700

    Start adding node-typescript debugger (not working yet)

--- neovim/.config/nvim/lua/plugins/dap.lua
+++ neovim/.config/nvim/lua/plugins/dap.lua
@@ -4,6 +4,46 @@ if not dap_present then
     return
 end
 
+dap.adapters.typescript = {
+    type = 'executable',
+    command = 'ts-node',
+    args = { os.getenv('HOME') .. '/repos/vscode-node-debug2/out/src/nodeDebug.js', '--esm' }
+}
+
+dap.configurations.typescript = {
+    {
+        type = 'typescript',
+        request = 'launch',
+        name = 'Launch program (node2 with ts-node)',
+        cwd = vim.fn.getcwd(),
+        runtimeArgs = { '-r', 'ts-node/register' },
+        args = { '--inspect', '${file}' },
+        sourceMaps = true,
+        skipFiles = { '<node_internal>/**', 'node_modules/**' },
+    },
+    {
+        type = 'typescript',
+        request = 'attach',
+        name = 'Attatch to program (node2 with ts-node)',
+        cwd = vim.fn.getcwd(),
+        sourceMaps = true,
+        skipFiles = { '<node_internal>/**' },
+        port = 9229,
+    },
+    {
+        type = 'typescript',
+        request = 'launch',
+        name = 'Launch test (node2 with ts-node)',
+        cwd = vim.fn.getcwd(),
+        runtimeArgs = { '--inspect-brk', '${workspaceFolder}/node_modules/.bin/jest' },
+        runtimeExecutable = 'node',
+        args = { '${file}', '--runInBand', '--coverage', 'false' },
+        sourceMaps = true,
+        port = 9229,
+        skipFiles = { '<node_internal>/**', 'node_modules/**' },
+    },
+}
+
 local dap_vtext_present, dap_vtext = pcall(require, "nvim-dap-virtual-text")
 if not dap_vtext_present then
     return
