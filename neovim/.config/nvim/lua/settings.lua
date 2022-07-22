6be1b9cd8f850e243a50527cbd79220b3c8c7fe6
Marcin Paliwoda <paliwoda.marcin@zoho.com>
2022-07-22 12:21:23 +0200

    Set signcolumn=yes

--- neovim/.config/nvim/lua/settings.lua
+++ neovim/.config/nvim/lua/settings.lua
@@ -56,6 +56,8 @@ set.updatetime = 400
 set.undofile = true
 set.undodir = os.getenv("HOME") .. '/.config/nvim/undodir'
 
+set.signcolumn = "yes"
+
 vim.g.python3_host_prog = '~/.pyenv/versions/neovim3/bin/python'
 vim.g.did_load_filetypes = 1
 vim.g.loaded_perl_provider = 0
