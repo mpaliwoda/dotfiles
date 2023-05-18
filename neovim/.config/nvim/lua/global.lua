1f686268dcfd33c285683621d0030232d1140e2f
Marcin Paliwoda <paliwoda.marcin@zoho.com>
2023-05-18 14:38:46 +0200

    Update prequire func

--- neovim/.config/nvim/lua/global.lua
+++ neovim/.config/nvim/lua/global.lua
@@ -1,11 +1,17 @@
----@type fun(module_name: string, callback: fun(module: table)): nil
-function prequire(module_name, callback)
-    local present, module = pcall(require, module_name)
+---@alias SuccessCallback fun(module: table): nil
+---@alias FailureCallback fun(module_name: string): nil | nil
+---@type fun(module_name: string, success_callback: SuccessCallback, failure_callback?: FailureCallback): nil
+function prequire(module_name, success_callback, failure_callback)
+   local present, module = pcall(require, module_name)
 
-    if not present then
-        vim.notify("Module " .. module_name .. " not installed. Run `PlugInstall`.", "error")
-        return
-    end
+   failure_callback = failure_callback or function(modname)
+      vim.notify("Module " .. modname .. " not installed. Run `PlugInstall`.", "error")
+   end
 
-    callback(module)
+   if not present then
+      failure_callback(module_name)
+      return
+   end
+
+   success_callback(module)
 end
