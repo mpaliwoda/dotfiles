ba91d53993a674344cb0d3a291c8688846d8110c
Marcin Paliwoda <paliwoda.marcin@zoho.com>
2024-05-28 01:29:44 +0200

    (maint): updat notify settings

--- neovim/.config/nvim/lua/mpaliwoda/plugins/notify.lua
+++ neovim/.config/nvim/lua/mpaliwoda/plugins/notify.lua
@@ -1,9 +1,17 @@
 return {
     "rcarriga/nvim-notify",
-    opts = {
-        fps = 60,
-        timeout = 1000,
-        render = "compact",
-        stages = "slide",
-    }
+    dependencies = {
+        "nvim-telescope/telescope.nvim",
+        "nvim-lua/plenary.nvim",
+    },
+    config = function()
+        require("notify").setup({
+            fps = 60,
+            timeout = 1000,
+            render = "compact",
+            stages = "slide",
+        })
+
+        require("telescope").load_extension("notify")
+    end,
 }
