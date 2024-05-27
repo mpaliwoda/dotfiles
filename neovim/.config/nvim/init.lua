7538534007a9bcbd8ed3d0177412b2ba5e1f9b79
Marcin Paliwoda <paliwoda.marcin@zoho.com>
2024-05-27 10:39:24 +0200

    (maint): additional lazy options

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -2,5 +2,24 @@ require("mpaliwoda.lazy")
 require("mpaliwoda.settings")
 
 require("lazy").setup({
-    spec = "mpaliwoda.plugins"
+    spec = "mpaliwoda.plugins",
+    checker = {
+        enabled = true,
+        concurrency = 8,
+    },
+    performance = {
+        rtp = {
+            disabled_plugins = {
+                "gzip",
+                "netrwPlugin",
+                "tarPlugin",
+                "tohtml",
+                "tutor",
+                "zipPlugin",
+            },
+        },
+    },
+    ui = {
+        border = "rounded",
+    }
 })
