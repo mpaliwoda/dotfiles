93518de0dd404510f07a4895b578dc79c0fb3514
Marcin Paliwoda <paliwoda.marcin@zoho.com>
2024-05-28 20:56:58 +0200

    (zsh): do the smort stuff with defering pyenv/rbenv/nvm plugins

--- zsh/.config/zsh/user/utils.zsh
+++ zsh/.config/zsh/user/utils.zsh
@@ -5,15 +5,3 @@ __clean() {
 py_clean() {
     __clean "(__pycache__|\.pyc|\.pyo$|.mypy_cache|.pytest_cache|.benchmarks)"
 }
-
-pyenv() {
-    eval "$(command pyenv init -)"
-
-    pyenv "$@"
-}
-
-rbenv() {
-    eval "$(command rbenv init - zsh)"
-
-    rbenv "$@"
-}
