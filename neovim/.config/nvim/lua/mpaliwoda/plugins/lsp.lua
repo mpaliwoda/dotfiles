07b89c8e21e89479eba93bd218f26eb5ef5170a7
Marcin Paliwoda <paliwoda.marcin@zoho.com>
2024-05-29 23:22:50 +0200

    (nvim): revert lsp_saga, didn't enjoy the experience

--- neovim/.config/nvim/lua/mpaliwoda/plugins/lsp.lua
+++ neovim/.config/nvim/lua/mpaliwoda/plugins/lsp.lua
@@ -1,3 +1,14 @@
+local function toggle_diagnostics()
+    local toggled = true
+
+    local wrapped = function()
+        if toggled then vim.diagnostic.hide() else vim.diagnostic.show() end
+        toggled = not toggled
+    end
+
+    return wrapped
+end
+
 local sign = function(opts)
     vim.fn.sign_define(opts.name, {
         texthl = opts.name,
@@ -217,11 +228,19 @@ return {
                     vim.keymap.set(mode, lhs, rhs, opts)
                 end
 
+                map("n", "<leader>mgd", "<cmd>lua vim.lsp.buf.definition()<cr>")
+                map("n", "<leader>mgs", "<cmd>lua vim.lsp.buf.references()<cr>")
                 map("n", "<leader>mgi", "<cmd>lua vim.lsp.buf.implementation()<cr>")
                 map("n", "<leader>mgy", "<cmd>lua vim.lsp.buf.type_definition()<cr>")
+                map("n", "<leader>ren", "<cmd>lua vim.lsp.buf.rename()<cr>")
+                map("n", "[g", "<cmd>lua vim.diagnostic.goto_prev({ float = false })<cr>")
+                map("n", "]g", "<cmd>lua vim.diagnostic.goto_next({ float = false })<cr>")
+                map("n", "<F2>", "<cmd>lua vim.lsp.codelens.run()<cr>")
 
+                map("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>")
                 map("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<cr>")
                 map("n", "<M-h>", "<cmd>lua vim.diagnostic.open_float()<cr>")
+                map("n", "<M-d>", toggle_diagnostics())
 
                 map("n", "<leader>fmt", "<cmd>lua vim.lsp.buf.format({ async = true })<cr>")
                 map("v", "<leader>fmt", "<cmd>lua vim.lsp.buf.format({ async = true })<cr>")
