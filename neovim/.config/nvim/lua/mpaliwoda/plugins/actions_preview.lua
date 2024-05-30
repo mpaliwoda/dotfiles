4a8034da1739dd87f0c7ad2b8a6a288d7fad1d23
Marcin Paliwoda <paliwoda.marcin@zoho.com>
2024-05-30 10:22:21 +0200

    (nvim): setup actions_preview better ig

--- neovim/.config/nvim/lua/mpaliwoda/plugins/actions_preview.lua
+++ neovim/.config/nvim/lua/mpaliwoda/plugins/actions_preview.lua
@@ -1,6 +1,20 @@
 return {
     "aznhe21/actions-preview.nvim",
-    keys = {
-        { mode = { "v", "n" }, "<leader><space>", "<cmd>lua require('actions-preview').code_actions()<cr>" }
-    }
+    dependencies = {
+        "nvim-telescope/telescope.nvim",
+        "nvim-lua/plenary.nvim",
+    },
+    event = "LspAttach",
+    config = function()
+        local actions = require("actions-preview")
+
+        actions.setup({
+            highlight_command = {
+                require("actions-preview.highlight").delta(),
+            },
+            telescope = require("telescope.themes").get_dropdown({ initial_mode = "normal" })
+        })
+
+        vim.keymap.set({ "n", "v" }, "<leader><space>", "<cmd>lua require('actions-preview').code_actions()<cr>")
+    end,
 }
