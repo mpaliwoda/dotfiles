569a99daa69241ffa70f6ecd4cee5624f8d50c3b
Marcin Paliwoda <paliwoda.marcin@zoho.com>
2024-08-02 04:05:30 +0200

    (nvim): remove image.nvim

--- neovim/.config/nvim/lua/mpaliwoda/plugins/image.lua
+++
@@ -1,7 +0,0 @@
-package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?/init.lua"
-package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?.lua"
-
-return {
-    "3rd/image.nvim",
-    opts = {},
-}
