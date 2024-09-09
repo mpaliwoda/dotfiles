92fa611682af342f40b9b734cd1e3cd882a7e7c1
Marcin Paliwoda <paliwoda.marcin@zoho.com>
2024-09-09 13:11:39 +0200

    (nvim): add image.nvim again, enable image support in leetcode plugin, add markdown plugin

---
+++ neovim/.config/nvim/lua/mpaliwoda/plugins/markdown.lua
@@ -0,0 +1,9 @@
+return {
+    'MeanderingProgrammer/markdown.nvim',
+    dependencies = {
+        'nvim-treesitter/nvim-treesitter',
+        'nvim-tree/nvim-web-devicons',
+    },
+    opts = {},
+    ft = { "md", "markdown" },
+}
