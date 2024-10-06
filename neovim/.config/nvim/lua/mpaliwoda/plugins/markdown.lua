ab5558b7bd19cc474655c2f199155e376a9bb5ed
Marcin Paliwoda <paliwoda.marcin@zoho.com>
2024-10-06 12:14:59 +0200

    (nvim): customize markview

--- neovim/.config/nvim/lua/mpaliwoda/plugins/markdown.lua
+++ neovim/.config/nvim/lua/mpaliwoda/plugins/markdown.lua
@@ -1,9 +1,21 @@
 return {
     "OXY2DEV/markview.nvim",
     lazy = false,
-    opts = {},
     dependencies = {
         "nvim-treesitter/nvim-treesitter",
         "nvim-tree/nvim-web-devicons"
-    }
+    },
+    config = function()
+        local presets = require("markview.presets");
+        local markview = require("markview")
+
+        markview.setup({
+            headings = vim.tbl_deep_extend('force', presets.headings.slanted, presets.horizontal_rules.double),
+            checkboxes = presets.checkboxes.nerd,
+        });
+
+        vim.keymap.set("n", "<M-m>", function() markview.commands.toggleAll() end)
+        vim.keymap.set("n", "<M-h>i", function() require("markview.extras.headings").increase() end)
+        vim.keymap.set("n", "<M-h>o", function() require("markview.extras.headings").decrease() end)
+    end
 }
