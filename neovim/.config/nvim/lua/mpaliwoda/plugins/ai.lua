21a3fcd2664dc79e4f720b4e02460ece342369ca
Marcin Paliwoda <paliwoda.marcin@zoho.com>
2024-11-27 11:36:46 +0100

    (feat): remove image.nvim, add gemini + toggling providers for avante

--- neovim/.config/nvim/lua/mpaliwoda/plugins/ai.lua
+++ neovim/.config/nvim/lua/mpaliwoda/plugins/ai.lua
@@ -11,15 +11,37 @@ return {
         "MunifTanjim/nui.nvim",
         "nvim-tree/nvim-web-devicons",
     },
-    opts = {
-        provider = "ollama",
-        vendors = {
-            ollama = {
-                __inherited_from = "openai",
-                api_key_name = "",
-                endpoint = "http://127.0.0.1:11434/v1",
-                model = "qwen2.5-coder:7b-instruct",
+    config = function()
+        require("avante_lib").load()
+        require("avante").setup({
+            provider = "gemini",
+            auto_suggestions_provider = "gemini",
+            vendors = {
+                ollama = {
+                    __inherited_from = "openai",
+                    api_key_name = "",
+                    endpoint = "http://127.0.0.1:11434/v1",
+                    model = "qwen2.5-coder:7b-instruct",
+                },
             },
-        },
-    }
+        })
+
+        vim.api.nvim_create_user_command("ToggleProvider", function()
+            local current_provider = require("avante.config").provider
+
+            local toggle_to
+            if current_provider == "ollama" then
+                toggle_to = "gemini"
+            else
+                toggle_to = "ollama"
+            end
+
+            require("avante.config").override({
+                provider = toggle_to,
+                auto_suggestions_provider = toggle_to,
+            })
+
+            vim.notify("Toggled Avante to use: " .. toggle_to)
+        end, {})
+    end
 }
