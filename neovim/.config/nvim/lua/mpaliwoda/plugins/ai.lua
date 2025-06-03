f404aafa9b51e7b6216048655e3ce7c7f41f7df4
Marcin Paliwoda <paliwoda.marcin@zoho.com>
2025-06-03 11:01:35 +0200

    (nvim): migrate provides config in avante

--- neovim/.config/nvim/lua/mpaliwoda/plugins/ai.lua
+++ neovim/.config/nvim/lua/mpaliwoda/plugins/ai.lua
@@ -12,26 +12,14 @@ return {
     },
     opts = {
         provider = "gemini",
-        cursor_applying_provider = 'gemini',
-        gemini = {
-            model = "gemini-2.5-flash-preview-05-20",
-            max_tokens = 8192 * 4,
-        },
-        claude = {
-            max_tokens = 8192,
-            model = "claude-3-5-haiku-20241022",
-            -- model = "claude-sonnet-4-20250514",
-        },
-        behaviour = {
-            auto_suggestions = false,
-            auto_set_highlight_group = true,
-            auto_set_keymaps = true,
-            auto_apply_diff_after_generation = false,
-            support_paste_from_clipboard = false,
-            minimize_diff = false,
-            cursor_planning_mode = true,
-        },
-        vendors = {
+        providers = {
+            gemini = {
+                model = "gemini-2.5-flash-preview-05-20",
+                extra_request_body = {
+                    temperature = 0.5,
+                    max_tokens = 8192 * 4,
+                }
+            },
             groq = {
                 __inherited_from = "openai",
                 api_key_name = "GROQ_API_KEY",
@@ -43,8 +31,20 @@ return {
                 api_key_name = "MISTRAL_API_KEY",
                 endpoint = "https://api.mistral.ai/v1/",
                 model = "mistral-large-latest",
-                max_tokens = 4096,
+                extra_request_body = {
+                    max_tokens = 4096,
+                }
             },
+
+        },
+        disabled_tools = { "git_commit" },
+        behaviour = {
+            auto_suggestions = false,
+            auto_set_highlight_group = true,
+            auto_set_keymaps = true,
+            auto_apply_diff_after_generation = false,
+            support_paste_from_clipboard = false,
+            minimize_diff = false,
         },
     }
 }
