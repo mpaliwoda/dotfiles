7d1c14cdb685a286722ac8d704b51b9d0fddb4ba
Marcin Paliwoda <paliwoda.marcin@zoho.com>
2025-06-04 12:42:55 +0200

    (nvim): simplify fsharp setup

--- neovim/.config/nvim/lua/mpaliwoda/plugins/ionide.lua
+++ neovim/.config/nvim/lua/mpaliwoda/plugins/ionide.lua
@@ -3,7 +3,7 @@ return {
     opts = {},
     ft = { "fsharp" },
     init = function()
-        vim.g["fsharp#lsp_auto_setup"] = 0
+        vim.g["fsharp#lsp_auto_setup"] = 1
         vim.g["fsharp#workspace_mode_peek_deep_level"] = 3
     end,
 }
--- neovim/.config/nvim/lua/mpaliwoda/plugins/none_ls.lua
+++ neovim/.config/nvim/lua/mpaliwoda/plugins/none_ls.lua
@@ -6,79 +6,30 @@ return {
             "nvimtools/none-ls-extras.nvim",
         },
         event = { "BufReadPre", "BufNewFile" },
-        opts = function()
-            local fsharplint = {
-                name = "FSharpLint",
-                method = require("null-ls").methods.DIAGNOSTICS,
-                filetypes = { "fsharp" },
-                generator = require("null-ls").generator({
-                    command = "dotnet-fsharplint",
-                    args = { "lint", "--file-type=file", "$FILENAME" },
-                    from_stderr = true,
-                    format = "raw",
-                    check_exit_code = function(code)
-                        return code <= 1
-                    end,
-                    on_output = function(params, done)
-                        local out = params.output
-
-                        if not out then
-                            done(nil)
-                            return
-                        end
-
-                        local sanitized = out:gsub("==========.-==========\n?", "")
-                        local diagnostics = {}
-
-                        for raw_diagnostic in sanitized:gmatch("(.-)%-%-%-+\n") do
-                            local m = {
-                                raw_diagnostic:match(
-                                    "(.-)\nError on line (%d+) starting at column (%d+)\n(.-\nSee.-%/(%w+%d+)%.html)"
-                                ),
-                            }
-                            vim.notify(vim.inspect(m))
-
-                            table.insert(diagnostics, {
-                                message = m[1] .. "\n" .. m[4],
-                                row = m[2],
-                                col = m[3],
-                                code = m[5],
-                                source = "FSharpLint",
-                                filename = params.bufname,
-                                severity = require("null-ls.helpers").diagnostics.severities.warning,
-                            })
-                        end
-
-                        done(diagnostics)
-                    end,
+        opts = {
+            sources = {
+                require("null-ls").builtins.diagnostics.mypy.with({
+                    prefer_local = ".venv/bin",
                 }),
-            }
-            return {
-                sources = {
-                    require("null-ls").builtins.diagnostics.mypy.with({
-                        prefer_local = ".venv/bin",
-                    }),
-                    require("null-ls").builtins.diagnostics.djlint.with({
-                        prefer_local = ".venv/bin",
-                        filetypes = { "htmldjango", "jinja", "jinja.html", "j2" },
-                    }),
-                    require("null-ls").builtins.formatting.djlint.with({
-                        prefer_local = ".venv/bin",
-                        filetypes = { "htmldjango", "jinja", "jinja.html", "j2" },
-                    }),
-                    require("none-ls.diagnostics.ruff").with({
-                        prefer_local = ".venv/bin",
-                    }),
-                    require("none-ls.formatting.ruff").with({
-                        prefer_local = ".venv/bin",
-                    }),
-                    require("none-ls.formatting.ruff_format").with({
-                        prefer_local = ".venv/bin",
-                    }),
-                    fsharplint,
-                },
-            }
-        end,
+                require("null-ls").builtins.diagnostics.djlint.with({
+                    prefer_local = ".venv/bin",
+                    filetypes = { "htmldjango", "jinja", "jinja.html", "j2" },
+                }),
+                require("null-ls").builtins.formatting.djlint.with({
+                    prefer_local = ".venv/bin",
+                    filetypes = { "htmldjango", "jinja", "jinja.html", "j2" },
+                }),
+                require("none-ls.diagnostics.ruff").with({
+                    prefer_local = ".venv/bin",
+                }),
+                require("none-ls.formatting.ruff").with({
+                    prefer_local = ".venv/bin",
+                }),
+                require("none-ls.formatting.ruff_format").with({
+                    prefer_local = ".venv/bin",
+                }),
+            },
+        }
     },
     {
         "jay-babu/mason-null-ls.nvim",
