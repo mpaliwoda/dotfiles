85f9aca223e712c28bbd3006bfddcb88e5c8cf3d
Marcin Paliwoda <marcin.paliwoda@playbookengineering.com>
2025-10-03 10:53:46 +0200

    (nvim): markview update

--- neovim/.config/nvim/lua/mpaliwoda/plugins/markdown.lua
+++ neovim/.config/nvim/lua/mpaliwoda/plugins/markdown.lua
@@ -5,40 +5,67 @@ return {
         "nvim-treesitter/nvim-treesitter",
         "nvim-tree/nvim-web-devicons",
     },
-    opts = {
-        markdown = {
-            code_blocks = {
-                sign = false,
-            },
-        },
-        markdown_inline = {
-            checkboxes = {
-                ["-"] = {
-                    hl = "MarkviewCheckboxPending",
-                    text = "",
-                },
-                checked = {
-                    hl = "MarkviewCheckboxChecked",
-                    scope_hl = "MarkviewCheckboxStriked",
-                    text = "",
-                },
-                enable = true,
-                o = {
-                    hl = "MarkviewCheckboxCancelled",
-                    text = "",
-                },
-                unchecked = {
-                    hl = "MarkviewCheckboxUnchecked",
-                    text = "",
-                },
-                ["~"] = {
-                    hl = "MarkviewCheckboxProgress",
-                    text = "",
+    config = function()
+        require("markview").setup({
+            markdown = {
+                code_blocks = {
+                    sign = false,
                 },
             },
-        },
-        preview = {
-            filetypes = { "markdown", "md", "rmd", "quarto", "Avante" },
-        },
-    },
+            markdown_inline = {
+                checkboxes = {
+                    ["-"] = {
+                        hl = "MarkviewCheckboxPending",
+                        text = "",
+                    },
+                    checked = {
+                        hl = "MarkviewCheckboxChecked",
+                        scope_hl = "MarkviewCheckboxStriked",
+                        text = "",
+                    },
+                    enable = true,
+                    o = {
+                        hl = "MarkviewCheckboxCancelled",
+                        text = "",
+                    },
+                    unchecked = {
+                        hl = "MarkviewCheckboxUnchecked",
+                        text = "",
+                    },
+                    ["~"] = {
+                        hl = "MarkviewCheckboxProgress",
+                        text = "",
+                    },
+                },
+            },
+            preview = {
+                filetypes = { "markdown", "md", "rmd", "quarto", "Avante" },
+            },
+            experimental = {
+                check_rtp_message = false
+            },
+        })
+
+        require("markview.extras.checkboxes").setup({
+            default = "X",
+            remove_style = "disable",
+            states = {
+                { " ", "/", "X" },
+                { "<", ">" },
+                { "?", "!", "*" },
+                { '"' },
+                { "l", "b", "i" },
+                { "S", "I" },
+                { "p", "c" },
+                { "f", "k", "w" },
+                { "u", "d" }
+            }
+        })
+
+        require("markview.extras.headings").setup();
+
+        vim.keymap.set("n", "<Leader>cb", "<Cmd>Checkbox toggle<cr>", { silent = true })
+        vim.keymap.set("n", "<Leader>h=", "<Cmd>Heading increase<cr>", { silent = true })
+        vim.keymap.set("n", "<Leader>h-", "<Cmd>Heading decrease<cr>", { silent = true })
+    end,
 }
