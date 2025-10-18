fe2ebd065a808129a5fd7386325a111288a1cb78
Marcin Paliwoda <paliwoda.marcin@zoho.com>
2025-10-18 09:30:22 +0200

    (nvim): remove tailwind tools since they're unmaintained

--- neovim/.config/nvim/lua/mpaliwoda/plugins/tailwind_tools.lua
+++
@@ -1,6 +0,0 @@
-return {
-    "luckasRanarison/tailwind-tools.nvim",
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
-    opts = {},
-    ft = { "html", "jinja", "j2", "htmldjango", "jsx", "tsx" },
-}
