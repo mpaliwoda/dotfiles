1e3ad62477241f19f45281ad2212eb2167a35884
Marcin Paliwoda <paliwoda.marcin@zoho.com>
2026-02-05 18:16:44 +0100

    nvim: treesitter

--- neovim/.config/nvim/lua/mpaliwoda/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/mpaliwoda/plugins/treesitter.lua
@@ -5,76 +5,58 @@ return {
         { "nvim-treesitter/nvim-treesitter-textobjects", lazy = true },
     },
     config = function()
-        local treesitter = require("nvim-treesitter.configs")
+        require("nvim-treesitter").setup()
 
-        treesitter.setup({
-            sync_install = false,
-            ensure_installed = {},
-            modules = {},
-            ignore_install = {},
-            highlight = {
+        require("nvim-treesitter-textobjects").setup({
+            swap = {
                 enable = true,
+                swap_next = {
+                    ["<M-L>"] = "@parameter.inner",
+                },
+                swap_previous = {
+                    ["<M-H>"] = "@parameter.inner",
+                },
             },
-            auto_install = true,
-            incremental_selection = {
+            move = {
                 enable = true,
+                set_jumps = true,
+                goto_next_start = {
+                    ["]p"] = "@parameter.inner",
+                    ["]m"] = "@function.outer",
+                    ["]]"] = "@class.outer",
+                },
+                goto_next_end = {
+                    ["]M"] = "@function.outer",
+                    ["]["] = "@class.outer",
+                },
+                goto_previous_start = {
+                    ["[p"] = "@parameter.inner",
+                    ["[m"] = "@function.outer",
+                    ["[["] = "@class.outer",
+                },
+                goto_previous_end = {
+                    ["[M"] = "@function.outer",
+                    ["[]"] = "@class.outer",
+                },
+            },
+            select = {
+                enable = true,
+                lookahead = true,
                 keymaps = {
-                    init_selection = "<M-n>",
-                    node_incremental = "<M-]>",
-                    node_decremental = "<M-[>",
-                },
-            },
-            textobjects = {
-                swap = {
-                    enable = true,
-                    swap_next = {
-                        ["<M-L>"] = "@parameter.inner",
-                    },
-                    swap_previous = {
-                        ["<M-H>"] = "@parameter.inner",
-                    },
-                },
-                move = {
-                    enable = true,
-                    set_jumps = true,
-                    goto_next_start = {
-                        ["]p"] = "@parameter.inner",
-                        ["]m"] = "@function.outer",
-                        ["]]"] = "@class.outer",
-                    },
-                    goto_next_end = {
-                        ["]M"] = "@function.outer",
-                        ["]["] = "@class.outer",
-                    },
-                    goto_previous_start = {
-                        ["[p"] = "@parameter.inner",
-                        ["[m"] = "@function.outer",
-                        ["[["] = "@class.outer",
-                    },
-                    goto_previous_end = {
-                        ["[M"] = "@function.outer",
-                        ["[]"] = "@class.outer",
-                    },
-                },
-                select = {
-                    enable = true,
-                    lookahead = true,
-                    keymaps = {
-                        ["aF"] = "@function.outer",
-                        ["iF"] = "@function.inner",
-                        ["ac"] = "@conditional.outer",
-                        ["ic"] = "@conditional.inner",
-                        ["aa"] = "@parameter.outer",
-                        ["ia"] = "@parameter.inner",
-                        ["av"] = "@variable.outer",
-                        ["iv"] = "@variable.inner",
-                        ["iC"] = "@class.inner",
-                        ["aC"] = "@class.outer",
-                        ["il"] = "@loop.inner",
-                        ["al"] = "@loop.outer",
-                        ["af"] = "@call.outer",
-                        ["if"] = "@call.inner",
-                    },
+                    ["aF"] = "@function.outer",
+                    ["iF"] = "@function.inner",
+                    ["ac"] = "@conditional.outer",
+                    ["ic"] = "@conditional.inner",
+                    ["aa"] = "@parameter.outer",
+                    ["ia"] = "@parameter.inner",
+                    ["av"] = "@variable.outer",
+                    ["iv"] = "@variable.inner",
+                    ["iC"] = "@class.inner",
+                    ["aC"] = "@class.outer",
+                    ["il"] = "@loop.inner",
+                    ["al"] = "@loop.outer",
+                    ["af"] = "@call.outer",
+                    ["if"] = "@call.inner",
                 },
             },
         })
